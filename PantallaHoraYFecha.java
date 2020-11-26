45001c692bc37dbb1a0f9ebfba5a00906bfc5670
nicolas <nicalpucci@gmail.com>
2020-11-26 13:30:19 +0100

    03 metodos agregados, funciona todo

--- PantallaHoraYFecha.java
+++ PantallaHoraYFecha.java
@@ -21,8 +21,42 @@ public class PantallaHoraYFecha{
     public PantallaHoraYFecha(){
         pantallaHoras = new PantallaDosDigitos(0, 24);
         pantallaMinutos = new PantallaDosDigitos(0, 60);
-        pantallaDia = new PantallaDosDigitos(1, 30);
-        pantallaMes = new PantallaDosDigitos(1, 12);
-        pantallaAno = new PantallaDosDigitos(1, 99);
+        pantallaDia = new PantallaDosDigitos(1, 31);
+        pantallaMes = new PantallaDosDigitos(1, 13);
+        pantallaAno = new PantallaDosDigitos(1, 100);
+    }
+    
+    /**
+     * Devuelve la hora actual de esta pantalla en el formato 
+     * HH:MM dd-mm-aa
+     */
+    public String getFechaYHora() {
+        return pantallaHoras.getTextoDeLaPantalla() + ":" + pantallaMinutos.getTextoDeLaPantalla() + " "
+        + pantallaDia.getTextoDeLaPantalla() + "-" + pantallaMes.getTextoDeLaPantalla() + "-" +
+        pantallaAno.getTextoDeLaPantalla();
+    }       
+    
+    /**
+     * Avanza el tiempo un minuto
+     */
+    public void AvanzarMinuto(){
+        pantallaMinutos.incrementaValorAlmacenado();
+        if (pantallaMinutos.getValorAlmacenado() == 0) {
+            pantallaHoras.incrementaValorAlmacenado();
+            pantallaDia.incrementaValorAlmacenado();
+            pantallaMes.incrementaValorAlmacenado();
+            pantallaAno.incrementaValorAlmacenado();
+        }
+    }
+    
+    /**
+     * Fija la fecha y la hora con los números que se requieran 
+     */
+    public void setTiempo(int horas, int minutos, int dia, int mes, int ano) {
+        pantallaHoras.setValorAlmacenado(horas);
+        pantallaMinutos.setValorAlmacenado(minutos);
+        pantallaDia.setValorAlmacenado(dia);
+        pantallaMes.setValorAlmacenado(mes);
+        pantallaAno.setValorAlmacenado(ano);
     }
 }
